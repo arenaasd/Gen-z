283a15e8d536b60489fb25e355ac8f9a2acaf7c7
Arsalankhan1218 <90974742+arenaasd@users.noreply.github.com>
2025-03-03 01:30:30 +0500

    Update mongoose-connection.js

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const debug = require('debug')('development:mongoose')
+const debug = require('debug')('development:mongoose');
 
-mongoose.connect(`${config.get("MONGODB_URI")}/gen-z`).then(function(){
-    debug("connected to db");
-}).catch(function(err){
-    debug(err);
-});
+// MongoDB URI should be a string, so wrap it in quotes
+const mongoUri = "mongodb+srv://akgamerz397:nadanak420@cluster0.rcjgs.mongodb.net/gen-z";
 
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        debug("Connected to MongoDB Atlas");
+    })
+    .catch((err) => {
+        debug(err);
+    });
 
 module.exports = mongoose.connection;
