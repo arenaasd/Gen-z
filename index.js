86571954394e0e3e52b3f687b7c884803557ccbd
Arsalankhan1218 <90974742+arenaasd@users.noreply.github.com>
2025-03-02 23:31:23 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const flash = require('connect-flash');
+const expressSession = require('express-session');
+const cookieparser = require('cookie-parser');
+const ownersroute = require('../routes/ownerRoutes');
+const registerroute = require('../routes/registerRoutes');
+const productsroute = require('../routes/productsRoutes');
+const db = require('../config/mongoose-connection');
+const loginroute = require('../routes/loginRoutes');
+const shop = require('../routes/index');
+require('dotenv').config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
+app.set('view engine', 'ejs');
+app.use('/user/register', express.static(path.join(__dirname, '../build')));
+app.use(cookieparser());
+app.use(
+    expressSession({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+app.use(flash());
+
+app.use('/owner', ownersroute);
+app.use('/user', registerroute);
+app.use('/product', productsroute);
+app.use('/user', loginroute);
+app.use('/', shop);
+
+// Wrap express app with Vercel's handler
+module.exports = (req, res) => {
+  app(req, res);
+};
